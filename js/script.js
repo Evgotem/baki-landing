b0a8142a4b9bd12ac3c4ca474f91933c53dcb19b
Evgotem <57010853+Evgotem@users.noreply.github.com>
2020-12-06 01:44:36 +0400

    добавлены скрипты для поэкранной прокрутки страницы и стили

--- js/script.js
+++ js/script.js
@@ -1,3 +1,63 @@
 let pageSlider = new Swiper('.page',{
-   wrapperClass: 'page__wrapper'
+   // свои классы
+   wrapperClass: 'page__wrapper',
+   slideClass: 'page__screen',
+
+   // вертикальный слайдер
+   direction: 'vertical',
+
+   // количество слайдов для показа
+   slidesPerView: 'auto',
+
+   // включаем параллакс
+   parallax: true,
+
+   // управление клавиатурой
+   keyboard: {
+      // включить\выключить
+      enabled: true,
+      // включить\выключить только когда слайдер в пределах вьюпорта
+      onlyInViewport: true,
+      // включить\выключить управление клавишами pageUp\pageDown
+      pageUpDown: true,
+   },
+
+   // управление колесом мыши
+   mousewheel: {
+      // чувствительность калеса мыши
+      sensitivity: 1,
+      // класс объекта, на котором будет срабатывать прокрутка мышью.
+      // eventsTarget: '.image-slider'
+   },
+   // отключение функционала если слайдов меньше чем нужно
+   watchOverflow: true,
+
+   // скорость
+   speed: 500,
+
+   // обновить свайпер при изменении элементов слайдера
+   observer: true,
+
+   // обновить свайпер при изменении родительских элементов слайдера
+   observeParents: true,
+
+   // обновить свайпер при изменении дочерних элементов слайдера
+   observeSlideChildren: true,
+
+   // навигация
+   // буллеты, текущее положение, прогрессбар
+   pagination: {
+      el: '.page__pagination',
+      type: 'bullets',
+      clickable: true,
+      bulletClass: 'page__bullet',
+      bulletActiveClass: 'page__bullet_active',
+   },
+   // скролл
+   scrollbar: {
+      el:'.page__scroll',
+      dragClass: 'page__drag-scroll',
+      // возможность перетаскивать скролл
+      draggable: true
+   }
 });
