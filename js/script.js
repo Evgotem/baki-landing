684597389e06e290ba2315961221c0b57e9d1b37
Evgotem <57010853+Evgotem@users.noreply.github.com>
2020-12-16 19:00:41 +0400

    хедер + навигация
    свёрстан хедер, навигация в процессе

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let pageSlider = new Swiper('.page',{
+let pageSlider = new Swiper('.page', {
    // свои классы
    wrapperClass: 'page__wrapper',
    slideClass: 'page__screen',
@@ -55,9 +55,59 @@ let pageSlider = new Swiper('.page',{
    },
    // скролл
    scrollbar: {
-      el:'.page__scroll',
+      el: '.page__scroll',
       dragClass: 'page__drag-scroll',
       // возможность перетаскивать скролл
       draggable: true
+   },
+   // отключаем автоинизиализацию
+   init: false,
+   // события
+   on: {
+      // событие инициализации
+      init: function () {
+         menuSlider();
+      },
+      // событие смены слайда
+      slideChange: function () {
+         menuSliderRemove();
+         menuLinks[pageSlider.realIndex].classList.add('active')
+      }
    }
 });
+
+let menuLinks = document.querySelectorAll('.header__link');
+
+function menuSlider() {
+   if (menuLinks.length > 0) {
+      menuLinks[pageSlider.realIndex].classList.add('active');
+      for (let i = 0; i < menuLinks.length; i++) {
+         const menuLink = menuLinks[i];
+         menuLink.onclick = e => {
+            menuSliderRemove();
+            // сделать чтобы при нажатии на пункт меню слайдилось на определенный экран
+            if (menuLinks[0]) {
+               pageSlider.slideTo(1, 800);
+            } 
+            else if (i === 1) {
+               pageSlider.slideTo(4, 800);
+            } 
+            else if (i === 2) {
+               pageSlider.slideTo(6, 800);
+            } 
+            else if (i === 3) {
+            pageSlider.slideTo(9, 800);
+            }
+         menuLink.classList.add('active');
+         e.preventDefault();
+   }
+}
+    
+function menuSliderRemove() {
+   let menuLinkActive = document.querySelector('.header__link.active');
+   if (menuLinkActive) {
+      menuLinkActive.classList.remove('active');
+   }
+}
+
+pageSlider.init();
