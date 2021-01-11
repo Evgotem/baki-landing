5d469fdc5ecaecafe7dc4a2f641ddf33d1834b51
Evgotem <57010853+Evgotem@users.noreply.github.com>
2021-01-11 16:38:53 +0400

    префиксы расставлены

--- js/script.js
+++ js/script.js
@@ -236,3 +236,8 @@ new Swiper('.projects-slider', {
    }
 });
 
+let section = document.querySelectorAll('section');
+for (let i = 0; i <section.length; i++) {
+   let activeSection = document.querySelector('.'+section[i].classList[0]);
+   console.log(activeSection.style);
+}
